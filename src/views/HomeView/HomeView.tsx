6992c8cb34d752a117cfaf8a7530b6b9efbf3e91
Domina Sokol <domest32@gmail.com>
2023-10-02 16:47:42 +0200

    add mock schedule

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -1,31 +1,37 @@
 import { Flex } from '@radix-ui/themes';
-import { isToday } from 'date-fns';
+import { format, isToday } from 'date-fns';
 import schedule from '../../mocks/mock.json';
+import styles from './HomeView.module.css';
 
-const HomeView = () => (
-  <Flex height="100%" align="center" direction="column">
-    <Flex asChild direction="column">
-      <ol>
-        {schedule.map(({ location, datetime }) => (
-          <Flex asChild key={datetime}>
-            <li>{isToday(new Date(datetime)) ? `there is a game today at ${location}` : ''}</li>
-          </Flex>
-        ))}
-      </ol>
-    </Flex>
+const HomeView = () => {
+  const entries = schedule.filter((entry) => isToday(new Date(entry.datetime)));
 
-    <Flex asChild direction="column">
-      <ol>
-        {schedule.map(({ location, datetime }) => (
-          <Flex asChild key={datetime}>
-            <li>
-              {datetime} {location}
-            </li>
-          </Flex>
-        ))}
-      </ol>
+  return (
+    <Flex height="100%" align="center" direction="column">
+      <Flex direction="column" className={styles.current}>
+        Today&lsquo;s game:
+        {entries.length === 0
+          ? 'no games today'
+          : entries.map((entry) => (
+              <Flex key={entry.datetime}>
+                on at {entry.location} at {format(new Date(entry.datetime), 'HH:mm')}
+              </Flex>
+            ))}
+      </Flex>
+
+      <Flex asChild direction="column" gap="5" grow="1">
+        <ol>
+          {schedule.map(({ location, datetime }) => (
+            <Flex asChild key={datetime}>
+              <li>
+                {format(new Date(datetime), 'MM/dd/yyyy HH:mm')} at {location}
+              </li>
+            </Flex>
+          ))}
+        </ol>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
 
 export default HomeView;
