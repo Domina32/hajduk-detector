a6796c14ffa806df596b9abaf4323f36f7ff4ceb
Domina Sokol <domina.sokol@gmail.com>
2023-10-04 16:17:20 +0200

    Revert "change schedule layout"
    This reverts commit 002a00a9df6642864a8954c4df4976c1d090f3c1.

--- src/views/HomeView/partials/Schedule/Schedule.tsx
+++ src/views/HomeView/partials/Schedule/Schedule.tsx
@@ -1,5 +1,6 @@
-import { Table, Text } from '@radix-ui/themes';
+import { Box, Flex, Text } from '@radix-ui/themes';
 import { format } from 'date-fns';
+import styles from './Schedule.module.css';
 
 interface Game {
   datetime: string;
@@ -11,31 +12,32 @@ interface ScheduleProps {
 }
 
 const Schedule: React.FC<ScheduleProps> = ({ games }) => (
-  <Table.Root variant="surface">
-    <Table.Header>
-      <Table.Row>
-        <Table.ColumnHeaderCell>
-          <Text weight="medium">Date & time</Text>
-        </Table.ColumnHeaderCell>
-        <Table.ColumnHeaderCell>
-          <Text weight="medium">Location</Text>
-        </Table.ColumnHeaderCell>
-      </Table.Row>
-    </Table.Header>
-
-    <Table.Body>
+  <Flex asChild direction="column" gap="5" grow="1">
+    <ol>
+      <Flex asChild className={styles.header} pl="4" pr="4">
+        <li>
+          <Box grow="1" style={{ width: '70%' }}>
+            <Text weight="medium">Date & time</Text>
+          </Box>
+          <Box grow="1" style={{ width: '30%' }}>
+            <Text weight="medium">Location</Text>
+          </Box>
+        </li>
+      </Flex>
       {games.map(({ datetime, location }) => (
-        <Table.Row key={datetime}>
-          <Table.Cell>
-            <Text>{format(new Date(datetime), 'MM/dd/yyyy HH:mm')}</Text>
-          </Table.Cell>
-          <Table.Cell>
-            <Text>{location}</Text>
-          </Table.Cell>
-        </Table.Row>
+        <Flex asChild key={datetime}>
+          <li>
+            <Box grow="1" style={{ width: '70%' }}>
+              <Text>{format(new Date(datetime), 'MM/dd/yyyy HH:mm')}</Text>
+            </Box>
+            <Box grow="1" style={{ width: '30%' }}>
+              <Text>{location}</Text>
+            </Box>
+          </li>
+        </Flex>
       ))}
-    </Table.Body>
-  </Table.Root>
+    </ol>
+  </Flex>
 );
 
 export default Schedule;
