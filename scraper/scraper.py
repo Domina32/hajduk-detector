07b5fd0bbaf4c00984b01a26b62e05b0d3e39724
Domina Sokol <domina.sokol@gmail.com>
2023-10-06 12:56:58 +0200

    add python typing

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen, Request
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -10,7 +8,7 @@ import re
 url = "https://hajduk.hr/utakmice/raspored"
 
 
-def parse_date_months(text):
+def parse_date_months(text: str) -> str:
     rep = {
         "siječnja": "01",
         "veljače": "02",
@@ -32,7 +30,7 @@ def parse_date_months(text):
     return pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
 
 
-def parse_date_days(text):
+def parse_date_days(text: str) -> str:
     rep = {
         "1": "01",
         "2": "02",
@@ -54,35 +52,24 @@ def parse_date_days(text):
     return text
 
 
-# headers = {
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-# }
-# request = Request(url, headers=headers)
-# page = urlopen(request)
-# html = page.read().decode("utf-8")
-# soup = BeautifulSoup(html, "html.parser")
-
 options = Options()
-options.add_argument("--headless")
-driver = Chrome(options=options)
+options.add_argument("--headless")  # type: ignore
+browser = Chrome(options=options)
 
-driver.get(url)
-driver.implicitly_wait(10)
+browser.get(url)
+browser.implicitly_wait(10)
 
-driver.find_element(By.CLASS_NAME, "cookie-popup__close").click()
+browser.find_element(By.CLASS_NAME, "cookie-popup__close").click()
 
-data = []
+data: list[dict[str, str]] = []
 
-schedule_div = driver.find_element(By.ID, "raspored")
+schedule_div = browser.find_element(By.ID, "raspored")
 print(schedule_div.text.split("\n"))
 
-for element in driver.find_elements(By.CLASS_NAME, "disabled"):
+for element in browser.find_elements(By.CLASS_NAME, "disabled"):
     teams_and_location, date_time = (
         element.text.split("\n")[1].split("   ")[0].split(", ")
     )
-
-    # "17. velja\u010de u 15:00h"
-    # "2023-10-04T07:35:17Z"
 
     just_time = date_time.split(" ")[3][:-1] + ":00Z"
     partially_parsed_date = parse_date_months(date_time.split(" u ")[0]).split(".")[
@@ -114,5 +101,5 @@ with open(os.path.join(json_path, json_name), "w") as f:
     json.dump(data, f)
 
 
-driver.close()
+browser.close()
 input("press any key to exit")
